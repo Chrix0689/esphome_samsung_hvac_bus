9052b4f09fb601e674975602eb1d789e31cd37bc
Chrix0689 <88665601+Chrix0689@users.noreply.github.com>
2025-06-03 13:11:23 +1000

    Update __init__.py

--- components/samsung_ac/__init__.py
+++ components/samsung_ac/__init__.py
@@ -1,5 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome.components.sensor import CONF_FILTERS
 from esphome.components import uart, sensor, switch, select, number, climate
 from esphome.const import (
     CONF_ID,
