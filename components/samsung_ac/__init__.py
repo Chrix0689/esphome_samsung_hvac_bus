0aa436c95d255a4ff13f96b04d4a1c18013f0aed
Chrix0689 <88665601+Chrix0689@users.noreply.github.com>
2025-06-03 13:08:14 +1000

    Update __init__.py

--- components/samsung_ac/__init__.py
+++ components/samsung_ac/__init__.py
@@ -143,12 +143,12 @@ CUSTOM_SENSOR_SCHEMA = sensor.sensor_schema().extend(
 
 def custom_sensor_schema(
     message: int,
-    unit_of_measurement: str = sensor.UNDEF,
-    icon: str = sensor.UNDEF,
-    accuracy_decimals: int = sensor.UNDEF,
-    device_class: str = sensor.UNDEF,
-    state_class: str = sensor.UNDEF,
-    entity_category: str = sensor.UNDEF,
+    unit_of_measurement: str = UNDEFINED,
+    icon: str = UNDEFINED,
+    accuracy_decimals: int = UNDEFINED,
+    device_class: str = UNDEFINED,
+    state_class: str = UNDEFINED,
+    entity_category: str = UNDEFINED,
 ):
     return sensor.sensor_schema(
         unit_of_measurement=unit_of_measurement,
